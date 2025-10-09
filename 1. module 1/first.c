ab604efe0cbb72500efef13d3b08b85394a1bc79
ariful <arifulislam321054@gmail.com>
2025-10-09 17:36:13 +0600

    print all types

--- 1. module 1/first.c
+++ 1. module 1/first.c
@@ -4,8 +4,16 @@ int main(){
 
     int num1 = 20;
     num1 = 30;
+    float f = 25.36;
+    char character = 'a';
 
     printf ("%d", num1);
+    printf ("\n");
+    printf ("%c", character);
+    printf ("\n");
+    printf ("%f", f);
+    printf ("\n");
+    printf ("%.2f", f); // 2 decimal porzonto print korbe
 
     return 0;
 }
