32bb9abb5ebb4f0ca1994c6470a05bd8861f8740
ariful <arifulislam321054@gmail.com>
2025-10-09 17:32:23 +0600

    data types

---
+++ 1. module 1/first.c
@@ -0,0 +1,26 @@
+#include<stdio.h> // standard input output
+int main(){
+    // printf("hello\nworld");
+
+    int num1 = 20;
+    num1 = 30;
+
+    printf ("%d", num1);
+
+    return 0;
+}
+
+
+/* 
+var type   space takes
+int ------- 4 byte
+float ----- 4 byte
+char ------ 1 byte
+*/
+
+/* 
+format specifier
+int -> %d
+float -> %f
+char -> %c
+*/
